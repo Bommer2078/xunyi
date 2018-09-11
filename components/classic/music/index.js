a3494b3cdf255be505c15740c870ca0d873a2b46
bommer <897694309@qq.com>
2018-09-11 08:40:36 +0800

    add music part

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -2,9 +2,11 @@
 Component({
     properties: {
         classicContent:String,
-        classicImg:String
+        classicImg:String,
     },
     data: {
-        imgUrl:'images/音乐。@2x.png'
+        imgUrl:'images/音乐。@2x.png',
+        playIng:'images/播放中.png',
+        pauseImg:'images/暂停中.png'
     }
 })
